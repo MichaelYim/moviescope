a5607dbb8018e34bba3ffa6ac1fdc98dee996f48
Tai Yin Michae Yim <mikeyimmy@gmail.com>
2014-08-11 17:20:22 +0800

    stack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Moviescope::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
+
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
